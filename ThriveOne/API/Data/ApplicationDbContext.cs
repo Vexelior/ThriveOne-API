0a30216bbdc903d6c7e903a420e6bf8fdc815605
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-05-20 12:15:52 -0700

    Add initial migration for database schema setup
    - Created initial migration files for the database schema.
    - Defined tables for AspNetRoles, AspNetUsers, DebtHistories, DebtImages, DebtInterestCharges, DebtPayments, DebtPreviousAmounts, DebtPreviousPercentages, Debts, Todos, WorkTasks, and their respective relationships.
    - Generated model snapshot for the current state of the database context.

---
+++ ThriveOne/API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using API.Entities.Debt;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+    public DbSet<Debt> Debts { get; set; }
+    public DbSet<PreviousAmount> DebtPreviousAmounts { get; set; }
+    public DbSet<History> DebtHistories { get; set; }
+    public DbSet<Payment> DebtPayments { get; set; }
+    public DbSet<PreviousPercentage> DebtPreviousPercentages { get; set; }
+    public DbSet<InterestCharge> DebtInterestCharges { get; set; }
+    public DbSet<Image> DebtImages { get; set; }
+    public DbSet<WorkTask> WorkTasks { get; set; }
+    public DbSet<Todo> Todos { get; set; }
+}
--- ThriveOne/API/Program.cs
+++ ThriveOne/API/Program.cs
@@ -1,8 +1,13 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

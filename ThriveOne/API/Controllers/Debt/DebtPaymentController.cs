7a5d13e64639fca6befd0c439f8ca72a09ed0c00
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-05-27 12:21:58 -0700

    feat: Implement CreateDebtPayment feature with handler and validation

---
+++ ThriveOne/API/Controllers/Debt/DebtPaymentController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Debt.Create.Payment;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Debt;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[ApiController]
+public class DebtPaymentController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateDebtPayment command)
+    {
+        try
+        {
+            var result = await mediator.Send(command);
+            if (result == null)
+            {
+                return NotFound("Failed to create debt payment.");
+            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ ThriveOne/Application/Features/Debt/Create/Payment/CreateDebtPaymentHandler.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using Persistence;
+
+namespace Application.Features.Debt.Create.Payment;
+
+public class CreateDebtPaymentHandler(ApplicationDbContext context) : IRequestHandler<CreateDebtPayment, Persistence.Entities.Debt.Payment>
+{
+    public async Task<Persistence.Entities.Debt.Payment> Handle(CreateDebtPayment request, CancellationToken cancellationToken)
+    {
+        CreateDebtPaymentValidator validator = new CreateDebtPaymentValidator();
+        ValidationResult validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        var debtPayment = new Persistence.Entities.Debt.Payment
+        {
+            Id = Guid.NewGuid(),
+            DebtId = request.DebtId,
+            Amount = request.Amount,
+            Date = request.Date
+        };
+        context.DebtPayments.Add(debtPayment);
+        await context.SaveChangesAsync(cancellationToken);
+        return debtPayment;
+    }
+}

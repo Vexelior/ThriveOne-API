88161c010b547db0f5bb5a9716a68a378f503a00
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-05-28 10:46:50 -0700

    feat: Implement CreateDebtPreviousAmount feature with handler and validation

---
+++ ThriveOne/API/Controllers/Debt/DebtPreviousAmountController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Debt.Create.PreviousAmount;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Debt;
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[ApiController]
+public class DebtPreviousAmountController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateDebtPreviousAmount command)
+    {
+        try
+        {
+            var result = await mediator.Send(command);
+
+            if (result == null)
+            {
+                return NotFound("Failed to create debt.");
+            }
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ ThriveOne/Application/Features/Debt/Create/PreviousAmount/CreateDebtPreviousAmountHandler.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using Persistence;
+
+namespace Application.Features.Debt.Create.PreviousAmount;
+
+public class CreateDebtPreviousAmountHandler(ApplicationDbContext context) : IRequestHandler<CreateDebtPreviousAmount, Persistence.Entities.Debt.PreviousAmount>
+{
+    public async Task<Persistence.Entities.Debt.PreviousAmount> Handle(CreateDebtPreviousAmount request, CancellationToken cancellationToken)
+    {
+        // Validate the request
+        CreateDebtPreviousAmountValidator validator = new CreateDebtPreviousAmountValidator();
+        ValidationResult validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        var previousAmount = new Persistence.Entities.Debt.PreviousAmount
+        {
+            Amount = request.Amount,
+            PercentageChange = request.PercentageChange,
+            DebtId = request.DebtId,
+            Description = request.Description,
+            DateAdded = DateTime.UtcNow
+        };
+        context.DebtPreviousAmounts.Add(previousAmount);
+        await context.SaveChangesAsync(cancellationToken);
+        return previousAmount;
+    }
+}

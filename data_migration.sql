b8392e200f8ad0990f059ebe0d86017c984e335c
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-06-11 11:15:02 -0700

    Update data_migration.sql

--- data_migration.sql
+++ data_migration.sql
@@ -11,19 +11,49 @@ EXEC sp_addlinkedsrvlogin
     @rmtuser = 'Alex', 
     @rmtpassword = 'Deadmau512#';
 
+-- Todos
 INSERT INTO ThriveOne.dbo.Todos (Id, Title, IsCompleted, Created, Completed, Description, Due)
 SELECT NEWID(), Title, IsCompleted, Created, Completed, Description, Due
 FROM [HOME_SERVER].Todo.dbo.Todos;
 
 
-
+-- Work Tasks
 INSERT INTO ThriveOne.dbo.WorkTasks (Id, Title, Description, CompletedAt, DueDate, Priority, Status, Markdown, HTML)
 SELECT NEWID(), Title, Description, CompletedAt, DueDate, Priority, Status, Markdown, HTML
 FROM [HOME_SERVER].WorkTaskTracker.dbo.Tasks;
 
 
-
+-- Debts
 INSERT INTO ThriveOne.dbo.Debts (Id, Creditor, Amount, RemainingAmount, PreviousAmount, Notes, DateAdded, DateEdited, PercentageChange, Type, Image, ImageId, ImageSource, InterestRate, LastPayment, LastPaymentDate, MinimumPayment)
 SELECT NEWID(), Creditor, Amount, RemainingAmount, PreviousAmount, Notes, DateAdded, DateEdited, PercentageChange, Type, Image, ImageId, ImageSource, InterestRate, LastPayment, LastPaymentDate, MinimumPayment
 FROM [HOME_SERVER].debts.dbo.Debts;
 
+
+-- Debt Images
+INSERT INTO ThriveOne.dbo.DebtImages(Id, Name, Source, Uploaded)
+SELECT NEWID(), Name, Source, Uploaded
+FROM [HOME_SERVER].debts.dbo.Images;
+
+
+-- Debt Charges
+INSERT INTO ThriveOne.dbo.DebtInterestCharges(Id, DebtId, Amount, Date, Description)
+SELECT NEWID(), DebtId, Amount, Date, Description
+FROM [HOME_SERVER].debts.dbo.InterestCharges;
+
+
+-- Debt Payments
+INSERT INTO ThriveOne.dbo.DebtPayments(Id, DebtId, Amount, Date)
+SELECT NEWID(), DebtId, Amount, Date
+FROM [HOME_SERVER].debts.dbo.Payments;
+
+
+-- Debt Previous Amounts
+INSERT INTO ThriveOne.dbo.DebtPreviousAmounts(Id, Amount, PercentageChange, DebtId, Description, DateAdded)
+SELECT NEWID(), Amount, PercentageChange, DebtId, Description, DateAdded
+FROM [HOME_SERVER].debts.dbo.PreviousAmounts;
+
+
+-- Debt Previous Percentages
+INSERT INTO ThriveOne.dbo.DebtPreviousPercentages(Id, DebtId, [Percent], Date)
+SELECT NEWID(), DebtId, Percentage, Date
+FROM [HOME_SERVER].debts.dbo.PreviousPercentages;

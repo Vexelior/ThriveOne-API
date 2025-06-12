326e5bcd7c9c307493f0849c45466b26e34646b4
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-06-12 08:59:23 -0700

    chore: remove not needed validation for todo create

--- ThriveOne/Application/Features/Todo/Create/CreateTodoValidator.cs
+++ ThriveOne/Application/Features/Todo/Create/CreateTodoValidator.cs
@@ -11,11 +11,6 @@ public class CreateTodoValidator : AbstractValidator<CreateTodo>
                                       .MaximumLength(100)
                                       .WithMessage("Title must not exceed 100 characters.");
 
-        RuleFor(x => x.Description).NotEmpty()
-                                            .WithMessage("Description is required.")
-                                            .MaximumLength(500)
-                                            .WithMessage("Description must not exceed 500 characters.");
-
         RuleFor(x => x.Created).NotEmpty()
                                         .WithMessage("Created date is required.")
                                         .Must(BeAValidDate)

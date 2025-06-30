2204fb30b582e09fe578e1c3fea996fb7f1d6e1d
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-06-29 20:14:51 -0700

    Add TimeOfDay field to Todo entity
    Introduces a new required string property 'TimeOfDay' to the Todo entity. Updates the database schema and migrations to support this new field.

--- ThriveOne/Persistence/Migrations/20250630031158_TodoTimeOfDay.Designer.cs
+++ ThriveOne/Persistence/Migrations/20250630031158_TodoTimeOfDay.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
@@ -11,9 +12,11 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250630031158_TodoTimeOfDay")]
+    partial class TodoTimeOfDay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -453,9 +456,6 @@ namespace Persistence.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<bool>("IsCompleted")
-                        .HasColumnType("bit");
-
-                    b.Property<bool>("IsDeleted")
                         .HasColumnType("bit");
 
                     b.Property<string>("Markdown")

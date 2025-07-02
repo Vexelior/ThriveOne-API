1d72286ceb28697d8bb96b1c6452eb56ef597607
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-07-01 18:05:50 -0700

    Add TimeOfDay field to UpdateTodo feature
    Introduces a new TimeOfDay property to the UpdateTodo command and updates the handler to set this value on the Todo entity. This allows specifying the time of day for a todo item during updates.

--- ThriveOne/Application/Features/Todo/Update/UpdateTodo.cs
+++ ThriveOne/Application/Features/Todo/Update/UpdateTodo.cs
@@ -2,7 +2,7 @@
 
 namespace Application.Features.Todo.Update;
 
-public class UpdateTodo(Guid id, string title, string description, DateTime? created, DateTime? completed, DateTime? due, bool isCompleted) : IRequest<Persistence.Entities.Todo.Todo>
+public class UpdateTodo(Guid id, string title, string description, DateTime? created, DateTime? completed, DateTime? due, bool isCompleted, string timeOfDay) : IRequest<Persistence.Entities.Todo.Todo>
 {
     public Guid Id { get; set; } = id;
     public string Title { get; set; } = title;
@@ -11,4 +11,5 @@ public class UpdateTodo(Guid id, string title, string description, DateTime? cre
     public DateTime? Completed { get; set; } = completed;
     public DateTime? Due { get; set; } = due;
     public bool IsCompleted { get; set; } = isCompleted;
+    public string TimeOfDay { get; set; } = timeOfDay;
 }
